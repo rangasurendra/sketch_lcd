7df087ee30b4853403dbce14d72422eb984782b0
Ranga Surendra <rwi@tiqri.com>
2018-09-08 22:21:14 +0530

    Add project files.

---
+++ Sketch_LCD/Sketch_LCD.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch_LCD.ino
+ Created:	9/8/2018 10:20:15 PM
+ Author:	Ranga Surendra
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
